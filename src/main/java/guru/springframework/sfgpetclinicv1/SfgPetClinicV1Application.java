cbe68aeb2f715ca75e0d9a04598d2ce66b1e56e4
benedict idonije <bidonije@googlemail.com>
2018-10-29 22:22:03 +0000

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinicv1/SfgPetClinicV1Application.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinicv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicV1Application.class, args);
+    }
+}
