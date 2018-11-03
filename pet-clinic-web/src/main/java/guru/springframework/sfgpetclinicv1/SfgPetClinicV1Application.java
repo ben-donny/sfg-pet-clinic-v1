52aea858bd2bb35372035b84f887906f6339757e
benedict idonije <bidonije@googlemail.com>
2018-11-03 11:09:54 +0000

    adding Index Page and controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicv1/SfgPetClinicV1Application.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicv1/SfgPetClinicV1Application.java
@@ -2,10 +2,8 @@ package guru.springframework.sfgpetclinicv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"guru.springframework.sfgpetclinicv1.model"})
 public class SfgPetClinicV1Application {
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicV1Application.class, args);
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicv1/controllers/indexcontroller.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinicv1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexcontroller {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public  String index(){
+
+        return "index";
+    }
+}
