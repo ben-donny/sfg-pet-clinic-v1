69c1e24d07410afa27cf7931abc36415a364dbfb
benedict idonije <bidonije@googlemail.com>
2018-11-03 11:42:10 +0000

    adding owners Index Page and controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicv1/controllers/Indexcontroller.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicv1/controllers/Indexcontroller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexcontroller {
+public class Indexcontroller {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public  String index(){
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicv1/controllers/OwnersController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinicv1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnersController {
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
