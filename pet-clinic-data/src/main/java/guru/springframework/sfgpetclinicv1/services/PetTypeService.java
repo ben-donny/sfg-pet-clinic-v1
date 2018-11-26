8bc6d304469b93aa133e671c99bb1ac397d0b2a7
benedict idonije <bidonije@googlemail.com>
2018-11-26 06:09:06 +0000

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv1/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicv1.services;
+
+import guru.springframework.sfgpetclinicv1.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
