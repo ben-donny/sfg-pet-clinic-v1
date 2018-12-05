e3d91f3fa20cfe945df38657f000cc7b6067058d
benedict idonije <bidonije@googlemail.com>
2018-12-05 06:24:16 +0000

    implemented spring data jpa for owner service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv1/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv1/repositories/PetRepository.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinicv1.repositories;
 
+import guru.springframework.sfgpetclinicv1.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetRepository extends CrudRepository<PetRepository, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> {
 }
